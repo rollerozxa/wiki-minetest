f41dc65e57c50b54df9d2219a99dc09c6e3c6c28
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-07-09 22:57:07 +0200

    Render wiki pages using markdown2formspec

--- wikilib.lua
+++ wikilib.lua
@@ -15,7 +15,6 @@ minetest.mkdir(wikilib.paths.plugins)
 minetest.mkdir(wikilib.paths.users)
 
 local function name_to_filename(name)
-
 	name = name:gsub("[^A-Za-z0-9-]", function(c)
 		if c == " " then
 			return "_"
@@ -24,13 +23,13 @@ local function name_to_filename(name)
 		end
 	end)
 	return name:lower()
-
 end
 wikilib.name_to_filename = name_to_filename
 
 local function get_page_path(name, player) --> path, is_file, allow_save
 
 	local allow_save = minetest.check_player_privs(player, {wiki=true})
+	local path
 
 	if name:sub(1, 1) == "." then
 		local text = wikilib.internal_pages[name] or wikilib.internal_pages[".NotFound_Internal"]
@@ -76,22 +75,10 @@ local function get_page_path(name, player) --> path, is_file, allow_save
 
 end
 
-local function find_links(lines) --> links
-	local links = { }
-	local links_n = 0
-	for _,line in ipairs(lines) do
-		for link in line:gmatch("%[(.-)%]") do
-			links_n = links_n + 1
-			links[links_n] = link
-		end
-	end
-	return links
-end
-
-local function load_page(name, player) --> text, links, allow_save
+local function load_page(name, player) --> text, allow_save
 	local text, allow_save = wikilib.plugin_handle_load(name, player)
 	if text then
-		return text, find_links(text:split("\n")), allow_save
+		return text, allow_save
 	end
 	local path, is_file, allow_save = get_page_path(name, player)
 	local f
@@ -111,8 +98,7 @@ local function load_page(name, player) --> text, links, allow_save
 	end
 	f:close()
 	local text = table.concat(lines, "\n")
-	local links = find_links(lines)
-	return text, links, allow_save
+	return text, allow_save
 end
 
 local function save_page(name, player, text)
@@ -136,26 +122,29 @@ end
 local esc = minetest.formspec_escape
 
 function wikilib.get_wiki_page_formspec(player, name)
-
 	if name == "" then name = "Main" end
 
-	local text, links, allow_save = load_page(name, player)
+	local text, allow_save = load_page(name, player)
 
-	local buttons, nbuttons = { }, 0
-	local bx, by = 12, 1.1
+	local toolbar = (allow_save
+			and "button[-.1,9.2;2.4,1;edit;Edit]"
+			or "button[-.1,9.2;2.4,1;edit;View Source]")
 
-	for i, link in ipairs(links) do
-		if i%15 == 0 then
-			bx = bx + 2
-			by = 1.1
-		end
-		link = esc(link)
-		nbuttons = nbuttons + 1
-		buttons[nbuttons] = (("button[%f,%f;2.1,0.5;page_%s;%s]")
-				:format(bx, by, link, link))
-		by = by + 0.65
-	end
-	buttons = table.concat(buttons)
+	return ("size[16,10]"
+		.. "label[-0.1,0;Page]"
+		.. "field[1.5,0.1;13,1;page;;"..esc(name).."]"
+		.. "button[14,0;1,0.5;go;Go]"
+		.. "button_exit[15,0;1,0.5;close;X]"
+		--.. "textarea[0.2,1.1;12,9;text;"..esc(name)..";"..esc(text).."]"
+		.. md2f.md2f(0.1, 1.1, 12, 9, text, "text")
+		.. toolbar
+	)
+end
+
+function wikilib.edit_wiki_page_formspec(player, name)
+	if name == "" then name = "Main" end
+
+	local text, allow_save = load_page(name, player)
 
 	local toolbar = (allow_save
 			and "button[-.1,9.2;2.4,1;save;Save]"
@@ -167,14 +156,17 @@ function wikilib.get_wiki_page_formspec(player, name)
 		.. "button[14,0;1,0.5;go;Go]"
 		.. "button_exit[15,0;1,0.5;close;X]"
 		.. "textarea[0.2,1.1;12,9;text;"..esc(name)..";"..esc(text).."]"
-		.. buttons
 		.. toolbar
 	)
-
 end
 
 function wikilib.show_wiki_page(player, name)
 	local fs = wikilib.get_wiki_page_formspec(player, name)
+	minetest.show_formspec(player, WIKI_FORMNAME, fs)
+end
+
+function wikilib.edit_wiki_page(player, name)
+	local fs = wikilib.edit_wiki_page_formspec(player, name)
 	minetest.show_formspec(player, WIKI_FORMNAME, fs)
 end
 
@@ -185,7 +177,7 @@ minetest.register_node("wiki:wiki", {
 		"[combine:16x16:0,0=default_bookshelf.png:0,0=wiki_wiki_front.png"
 	},
 	groups = { choppy=3, oddly_breakable_by_hand=2, flammable=3 },
-	sounds = default.node_sound_wood_defaults(),
+	--sounds = default.node_sound_wood_defaults(),
 	on_construct = function(pos)
 		local meta = minetest.get_meta(pos)
 		meta:set_string("infotext", "Wiki")
@@ -207,12 +199,14 @@ minetest.register_privilege("wiki_admin", {
 	give_to_singleplayer = false,
 })
 
-local BS = "default:bookshelf"
-local BSL = { BS, BS, BS }
-minetest.register_craft({
-	output = "wiki:wiki",
-	recipe = { BSL, BSL, BSL },
-})
+if default then
+	local BS = "default:bookshelf"
+	local BSL = { BS, BS, BS }
+	minetest.register_craft({
+		output = "wiki:wiki",
+		recipe = { BSL, BSL, BSL },
+	})
+end
 
 function wikilib.handle_formspec(player, formname, fields)
 	if (not formname) or (formname ~= WIKI_FORMNAME) then return end
@@ -226,19 +220,12 @@ function wikilib.handle_formspec(player, formname, fields)
 			wikilib.show_wiki_page(plname, fields.page)
 		end
 		return true
+	elseif fields.edit then
+		wikilib.edit_wiki_page(plname, fields.page)
+		return true
 	elseif fields.go then
 		wikilib.show_wiki_page(plname, fields.page)
 		return true
-	else
-		for k in pairs(fields) do
-			if type(k) == "string" then
-				local name = k:match("^page_(.*)")
-				if name then
-					wikilib.show_wiki_page(plname, name)
-					return true
-				end
-			end
-		end
 	end
 end
 
